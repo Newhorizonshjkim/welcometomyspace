f2554d25fd43a80eb234ed770ab5da3b088c349a
Newhorizonshjkim <75823349+Newhorizonshjkim@users.noreply.github.com>
2020-12-13 02:38:47 +0900

    version.1 test

---
+++ JS/login_script.js
@@ -0,0 +1,9 @@
+var LOGIN = document.getElementById('LOGIN');
+LOGIN.addEventListener('click', function(){
+    alert('히히');
+})
+
+var SIGNUP = document.getElementById('SIGNUP');
+SIGNUP.addEventListener('click', function(){
+    alert('이건 다음에 만들어 본다... 어렵당ㅠ');
+})
