168af6f19d7549de18ca0c33d4e243f034f95a1f
newhorizonshjkim <newhorizons@gmail.com>
2021-01-03 01:28:11 +0900

    login_script : id & pw match -> link "LOGIN.html"

--- JS/login_script.js
+++ JS/login_script.js
@@ -1,14 +1,41 @@
-var LOGIN = document.getElementById('LOGIN');
-LOGIN.addEventListener('click', function(){
-    alert('히히');
-})
 
-var SIGNUP = document.getElementById('SIGNUP');
-SIGNUP.addEventListener('click', function(){
-    alert('이건 다음에 만들어 본다... 어렵당ㅠ');
-})
 
-       
+
+// var LOGIN = document.getElementById('LOGIN');
+// LOGIN.addEventListener('click', function(){
+//     alert('히히');
+// })
+
+// var SIGNUP = document.getElementById('SIGNUP');
+// SIGNUP.addEventListener('click', function(){
+//     alert('이건 다음에 만들어 본다... 어렵당ㅠ');
+// })
+
+
+
+
+document.getElementById('LOGIN').onclick = function() {
+            var id = document.getElementById('id').value;
+            var password = document.getElementById('password').value;
+            if (id === "hjkim" && password === "hjkim"){
+                    alert("Welcome");
+                    location.href="INDEX.html";
+            } else {
+                    alert("Who are you?");
+                }
+}
+
+
+document.getElementById('SIGN UP').onclick = function() {
+            alert("Please wait");
+                
+}
+
+
+
+
+/*   <로그인폼2>
+
         // id = prompt('아이디를 입력해주세요.');
         // if(id=='hjkim'){
         //     password = prompt('비밀번호를 입력해주세요.');
@@ -20,3 +47,27 @@ SIGNUP.addEventListener('click', function(){
         // } else {
         //     alert('인증에 실패 했습니다.');
         // }
+*/
+
+
+
+
+//  <로그인폼3> 이거 왜 안되니
+
+// document.getElementById('LOGIN').onclick = function() {
+//             var id = document.getElementById('id').value;
+//             var password = document.getElementById('password').value;
+//             if (id === "hjkim" && password === "hjkim"){
+//                     alert("Welcome");
+//                     location.href="INDEX.html";
+//             } else if (document.getElementById('id').value; == ""){
+//                     alret("Please enter your id.");
+//                     document.getElementById('id').focus();
+//             } else if (password !=="hjkim"){
+//                     alret("Please enter your password.");
+//             } else {
+//                     alert("Who are you?");
+//                 }
+// }
+
+
